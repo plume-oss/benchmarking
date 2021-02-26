b3e179932dc0d19950e83b7459026121b9fad1ba
David Baker Effendi <davidbakereffendi@hotmail.com>
2021-02-26 10:29:41 +0200

    Simplified process reading and catching all cases of the status

--- src/main/scala/DockerManager.scala
+++ src/main/scala/DockerManager.scala
@@ -27,29 +27,28 @@ object DockerManager {
     dockerComposeUp.run(ProcessLogger(_ => ()))
     var status = false
     while (!status) {
+      Thread.sleep(2500)
       val healthCheck = Seq("docker", "inspect", "--format='{{json .State.Health}}'", dbName)
       try {
-        val rawResponse = healthCheck.lazyLines
-        breakable {
-          for (x <- rawResponse) {
-            val jsonRaw = x.substring(1, x.length - 1)
-            io.circe.parser.parse(jsonRaw) match {
-              case Left(failure) => logger.warn(failure.message, failure.underlying)
-              case Right(json) =>
-                json.\\("Status").head.asString match {
-                  case Some("unhealthy") => logger.info("Container is unhealthy")
-                  case Some("starting") => logger.info("Container is busy starting")
-                  case Some("healthy") => logger.info("Container is healthy! Proceeding...")
-                    status = true
-                    break
-                }
+        val x: String = healthCheck.!!.trim
+        val jsonRaw = x.substring(1, x.length - 1)
+        io.circe.parser.parse(jsonRaw) match {
+          case Left(failure) => logger.warn(failure.message, failure.underlying)
+          case Right(json) =>
+            json.\\("Status").head.asString match {
+              case Some("unhealthy") => logger.info("Container is unhealthy")
+              case Some("starting") => logger.info("Container is busy starting")
+              case Some("healthy") => logger.info("Container is healthy! Proceeding...")
+                status = true
+                break
+              case Some(x) =>  logger.info(s"Container is $x")
+              case None => logger.warn("Unable to obtain container health.")
             }
-          }
         }
       } catch {
         case _: StringIndexOutOfBoundsException => // Usually happens just as the services have been created
+        case e: IllegalAccessError => e.printStackTrace()
       }
-      Thread.sleep(2500)
     }
   }
 
