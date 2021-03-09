0b154393537f5c7571066e7d1efaf4f2e4b87b24
David Baker Effendi <davidbakereffendi@hotmail.com>
2021-03-09 15:16:36 +0200

    Replaced empty janusgraph all with core

--- src/main/scala/BenchmarkResult.scala
+++ src/main/scala/BenchmarkResult.scala
@@ -8,4 +8,14 @@ case class BenchmarkResult(
     databaseWrite: Long,
     databaseRead: Long,
     scpgPasses: Long
-)
+) {
+  override def toString: String = s"BenchmarkResult { " +
+    s"fileName=$fileName, " +
+    s"database=$database, " +
+    s"loadingAndCompiling=${loadingAndCompiling*Math.pow(10, -9)}s }, " +
+    s"unitGraphBuilding=${unitGraphBuilding*Math.pow(10, -9)}s, " +
+    s"databaseWrite=${databaseWrite*Math.pow(10, -9)}s, " +
+    s"databaseRead=${databaseRead*Math.pow(10, -9)}s, " +
+    s"scpgPasses=${scpgPasses*Math.pow(10, -9)}s, " +
+    " }"
+}
