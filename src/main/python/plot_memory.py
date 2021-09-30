19d846e2d1f588a358d82477b1c2edc7b11a0a99
David Baker Effendi <davidbakereffendi@hotmail.com>
2021-09-30 11:18:01 +0200

    Included memory level

--- src/main/python/plot_memory.py
+++ src/main/python/plot_memory.py
@@ -6,8 +6,9 @@ import seaborn as sns
 def plot(input_file):
     df = pd.read_csv(input_file, delimiter=',')
     df.info()
-    df['Data'] = df['Data'].apply(lambda x: x / 1024e3)
+    df['Memory'] = df['Memory'].apply(lambda x: x / 1024e3)
+    df = df.rename(columns={'Memory': 'Memory [Mb]'})
     sns.set(style="darkgrid")
     # Create the boxplot
-    sns.violinplot(x="Project", y="Data", hue="Database", cut=0, data=df)
+    sns.violinplot(x="Project", y="Memory [Mb]", hue="Database", cut=0, data=df)
     plt.show()
