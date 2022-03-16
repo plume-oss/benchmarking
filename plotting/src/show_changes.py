107fc141f7c129f0e21cd6299766307d59726a2d
David Baker Effendi <dbe@sun.ac.za>
2022-03-16 22:19:54 +0200

    Added a show changes script to view incremental stats

--- plotting/src/show_changes.py
+++ plotting/src/show_changes.py
@@ -6,8 +6,11 @@ import constants
 def plot(input_file):
     df = pd.read_csv(input_file, delimiter=',')
     df = df[df['PHASE'].str.contains("UPDATE")]
-    print(df.groupby(['FILE_NAME'])['CHANGED_CLASSES'].mean().astype(int).reset_index())
-    print(df.groupby(['FILE_NAME'])['CHANGED_METHODS'].mean().astype(int).reset_index())
+    cs = df.groupby(['FILE_NAME'])['CHANGED_CLASSES'].mean().astype(int).reset_index()
+    ms = df.groupby(['FILE_NAME'])['CHANGED_METHODS'].mean().astype(int).reset_index()
+    ms['CHANGED_CLASSES'] = cs['CHANGED_CLASSES']
+    ms["METHOD_PER_CLASS"] = (ms['CHANGED_METHODS'] / cs['CHANGED_CLASSES']).astype(int)
+    print(ms.head(9))
 
 
 if __name__ == '__main__':
