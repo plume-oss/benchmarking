399258a80a107fe9ab01e0e676dbfd5c2d7b961f
David Baker Effendi <dbe@sun.ac.za>
2022-04-01 16:51:15 +0200

    Removed outdated artifacts

--- experiments/dataflow_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in {1..5}
-do
-   ../../joernio/joern/joern --script src/main/cpgql/DataFlowQueries.sc --params cpgFile="./src/main/resources/cpgs/n$i.bin",outFile=./results/dataflow_queries.csv
-   ../../joernio/joern/joern --script src/main/cpgql/DataFlowQueries.sc --params cpgFile="./src/main/resources/cpgs/g$i.bin",outFile=./results/dataflow_queries.csv
-   ../../joernio/joern/joern --script src/main/cpgql/DataFlowQueries.sc --params cpgFile="./src/main/resources/cpgs/j$i.bin",outFile=./results/dataflow_queries.csv
-done
